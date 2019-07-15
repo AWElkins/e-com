90685611d5564bd96e4704e17f9f204b0ada54fc
Aaron Elkins <aaronelkins@Aarons-MBP.lan1>
2019-07-14 19:37:45 -0700

    Add sign up with email and password functionality

--- src/components/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/components/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './sign-in-and-sign-out.styles.scss';
 import SignIn from '../sign-in/sign-in.component';
+import SignUp from '../sign-up/sign-up.component';
 
 const SignInAndOut = () => {
-    return <div><SignIn /></div>
+    return <div className={ 'sign-in-and-sign-up' }>
+        <SignIn />
+        <SignUp />
+    </div>
 }
 
 export default SignInAndOut;
