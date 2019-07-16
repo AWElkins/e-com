b2a2dd6284a6b70138d6ba6ea03ffab6f7e31066
Aaron Elkins <aaronelkins@Aarons-MBP.lan1>
2019-07-15 20:14:04 -0700

    Added adding multiples of same items and utils for cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,16 +1,21 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
+    cartItems: [],
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case CartActionTypes.TOOGLE_CART_HIDDEN:
-            console.log("Fired");
             return {
                 ...state,
                 hidden: !state.hidden,
+            }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                cartItems: addItemToCart(state.cartItems, action.payload),
             }
         default:
             return state;
